343648197d81c79b141ec2bbfd031d85479ed7a7
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-10 15:06:43 -0500

    Initial Commit

---
+++ Hungry Developer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hungry Developer
+//
+//  Created by Ilgar Ilyasov on 10/10/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
